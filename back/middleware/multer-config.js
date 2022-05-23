005b5eaf76033458a5efb2a836220c35c1ccc2ea
ROUX <thomas.roux94@hotmail.fr>
2022-05-23 19:14:15 +0200

    multer

--- back/middleware/multer-config.js
+++ back/middleware/multer-config.js
@@ -16,10 +16,12 @@ const storage = multer.diskStorage({
         // création file name 
         const name = file.originalname.split(' ').join('_');
         //création de l'extension du fichier sur le mone type
+
         const extension = MIME_TYPES[file.mimetype];
         // génération du fichier
         callback(null, name + Date.now() + '.' + extension);
     }
 });
+
 // exportation du multer
 module.exports = multer({ storage: storage }).single('image');
